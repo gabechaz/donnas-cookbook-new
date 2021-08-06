4996f86a4ec9e2fa31d6089cf90d6987dba2f929
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-08-06 13:08:54 -0400

    pre backend shut down commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
         render json: User.find(params[:id])
     end
 
+
+    #This method is so the front end has a route to pull just the names of the recipes of any given user. 
     def books
         render json: User.find(params[:id]).recipe_names
     end
