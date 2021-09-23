8da2fe100218fae4b4b93508960c29a82002cd21
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-09-23 16:27:16 -0400

    added unique user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :recipes
     has_many :ingredients, through: :recipes
+    validates :username, uniqueness: true
 
     def recipe_info
         self.recipes.map{|recipe| {name: recipe.name, id: recipe.id}}
